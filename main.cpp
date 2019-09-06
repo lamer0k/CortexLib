77bc8744f5e711d14f7da37681633227e13e0954
Sergey Kolody <Sergey.Kolody@emerson.com>
2019-09-06 11:35:11 +0500

    STM32F411 folder added

--- main.cpp
+++ main.cpp
@@ -2,32 +2,42 @@
 #include "gpioaregisters.hpp" //for Gpioa
 #include "rccregisters.hpp"   //for RCC
 #include "tim1registers.hpp"  //for TIM1
-//#include "dioregisters.hpp"
-//#include "timera0registers.hpp"
 
 
 using namespace std ;
 
 int main()
 {
-//  auto test = DIO::PAIN::Get() ;
-//  auto res = DIO::P1IV::P1IVField::P1IV_2::IsSet() ;
- 
-//  res = DIO::P1IVPack<
-//          DIO::P1IV::P1IVField::P1IV_2,
-//          DIO::P1IV::P1IVField::P1IV_4
-//          >::IsSet() ;
-//  TIMER_A0::TAxCTL::ID::ID_0::Set() ;
   
-//  TIMER_A0::TAxCCR1::TAxR::Set(10U) ;
-//  auto res = Dio::P2Iv::P2IvField::P2Iv0::IsSet() ;
-//  res = Dio::P2IvPack<
-//          Dio::P2Iv::P2IvField::P2Iv0,
-//          Dio::P2Iv::P2IvField::P2Iv10
-//          >::IsSet() ;
-//  TimerA0::Taxctl::Id::Id0::Set();
+  RCC::AHB1ENR::GPIOAEN::Enable::Set() ;
   
-  RCC::AHB1ENR::GPIOAEN::Disable::Set() ;
+  GPIOA::MODERPack<
+          GPIOA::MODER::MODER15::Output,
+          GPIOA::MODER::MODER14::Analog
+  >::Set() ;
+
+  if (GPIOA::MODERPack<
+          GPIOA::MODER::MODER15::Output,
+          GPIOA::MODER::MODER14::Analog
+  >::IsSet() )
+  {
+    GPIOA::MODER::Set(0U) ;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   TIM1::CR1::CKD::Divider2::Set() ;
   if (TIM1::CR1::CKD::Divider2::IsSet())
@@ -54,15 +64,8 @@ int main()
   GPIOA::MODER::Set(2U) ;
   auto test = GPIOA::MODER::Get() ;
 
-  GPIOA::MODERPack<
-          GPIOA::MODER::MODER15::Output,
-          GPIOA::MODER::MODER14::Analog
-  >::Set() ;
+  
 
-  result = GPIOA::MODERPack<
-          GPIOA::MODER::MODER15::Output,
-          GPIOA::MODER::MODER14::Analog
-  >::IsSet() ; ;
 
   GPIOA::MODER::MODER15::Set(2U) ;
   test = GPIOA::MODER::MODER15::Get() ;
