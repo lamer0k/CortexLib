633a2fcdd92a1722b7e38a0a01c4451d52663129
Sergey Kolody <Sergey.Kolody@emerson.com>
2019-09-13 11:10:24 +0500

    Add files for students

---
+++ cmake-build-debug-iar/CMakeFiles/Registers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Registers.dir/main.cpp.obj"
+  "Registers.pdb"
+  "Registers.elf"
+  "Registers.elf.manifest"
+  "Registers.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Registers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
