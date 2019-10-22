603a8a9da228221f6517d3ddd54cf65595d5ae32
Sergey Kolody <Sergey.Kolody@emerson.com>
2019-10-22 18:41:43 +0500

    Add Milandr svd

---
+++ cmake-build-debug-iar/CMakeFiles/Registers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Registers.dir/main.cpp.obj"
+  "Registers.elf"
+  "Registers.elf.manifest"
+  "Registers.lib"
+  "Registers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Registers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
