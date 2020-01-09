22deb3f4429288af3ae7b04c8bb55453f3e010a6
Sergey Kolody <Sergey.Kolody@emerson.com>
2020-01-10 02:58:39 +0500

    font.h

---
+++ Application/Gui/Fonts/font.h
@@ -0,0 +1,31 @@
+/*******************************************************************************
+*  FILENAME: font5_8.h
+*
+*  DESCRIPTION: 
+*
+*  Copyright (c) 2015 by South Ural State Universaty
+*  Author: Sergey Kolody*
+******************************************************************************/
+#ifndef FONT_H
+#define FONT_H
+
+
+typedef struct
+{
+  std::uint16_t width;
+  std::uint16_t index;
+}tFontDescriptor;
+
+typedef struct 
+{
+  std::uint8_t height;		// ������ ��������
+  std::uint8_t firstIndex;
+  std::uint32_t charsCount;
+  std::uint8_t spaceSize;
+  const tFontDescriptor* fontIndex; //Index of Font
+  const std::uint8_t* fontTable;	// ������ ������ ������
+} tFont;
+
+
+
+#endif //FONT_H
