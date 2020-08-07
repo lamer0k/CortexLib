35e8d06f342f900d0d3811a35432007734f59ca7
lamer0k <sergey.kolody@gmail.com>
2020-08-07 13:20:22 +0500

    Fix issue with Mask

--- AbstractHardware/Registers/fieldvalue.hpp
+++ AbstractHardware/Registers/fieldvalue.hpp
@@ -5,20 +5,23 @@
 #pragma once
 
 #include <cstddef>           //for size_t
+#include <limits>       // for std::mnumeric_limits<>
 #include "fieldvaluebase.hpp" //for BitsFieldBase
 
-using namespace std ;
+using namespace std;
 
 //Класс для работы с битовыми полями. Добавился тип Base, который необходим для того, чтобы проверить, что
 //В регистре устанавливаются те битовые поля, которые допустимы для данного регистра
 //template<typename Reg, size_t offset, size_t size, typename AccessMode, typename Base, typename Reg::Type value>
-template<typename Field, typename Base, typename Field::Register::Type value>
-struct FieldValue: public FieldValueBase<Field, Base, value>
+template<typename Field, typename Base, typename Field::Register::Type value,
+				class = typename std::enable_if_t<std::is_unsigned<typename Field::Register::Type>::value>>
+struct FieldValue : public FieldValueBase<Field, Base, value>
 {
-  using Type = typename Field::Register::Type ;
-  constexpr static auto Mask = static_cast<Type>(static_cast<Type>(1U) << static_cast<Type>(Field::Size)) - static_cast<Type>(1U) ;
-  constexpr static auto Value = value ;
-  constexpr static auto Offset = Field::Offset ;
-  using BaseType = Base ;
-  using Access = typename Field::Access ;
-} ;
+		using Type = typename Field::Register::Type;
+		constexpr static auto Mask = (Field::Size <	sizeof(Type)) ? static_cast<Type>(static_cast<Type>(1U) << (static_cast<Type>(Field::Size)) - static_cast<Type>(1U)) :
+		                                     std::numeric_limits<Type>::max();;
+		constexpr static auto Value = value;
+		constexpr static auto Offset = Field::Offset;
+		using BaseType = Base;
+		using Access = typename Field::Access;
+};
--- AbstractHardware/Registers/registerfield.hpp
+++ AbstractHardware/Registers/registerfield.hpp
@@ -5,37 +5,52 @@
 #pragma once
 
 #include <cassert>      //for assert
+#include <limits>       // for std::mnumeric_limits<>
 //#include <atomicutils.hpp>
 #include "susudefs.hpp" //for __forceinline (#define __forceinline  _Pragma("inline=forced"))
 //#include "criticalsectionconfig.hpp" // for CriticalSection
 
 //Базовый класс для работы с битовыми полями регистров
-template<typename Reg, size_t offset, size_t size, typename AccessMode>
+template<typename Reg, size_t offset, size_t size, typename AccessMode, class = typename std::enable_if_t<std::is_unsigned<typename Reg::Type>::value>>
 struct RegisterField
 {
-  using RegType = typename Reg::Type ;
-  using Register = Reg ;
-  
-  static constexpr RegType Offset = offset ;
-  static constexpr RegType Size = size ;
-  static constexpr RegType Mask  = static_cast<RegType>((static_cast<RegType>(1U) << size) - static_cast<RegType>(1U)) ;
-  using Access = AccessMode ;
+		using RegType = typename Reg::Type;
+		using Register = Reg;
 
-  //Метод устанавливает значение битового поля, только в случае, если оно достпуно для записи
-  __forceinline template<typename T = AccessMode,
-          class = typename std::enable_if_t<std::is_base_of<ReadWriteMode, T>::value>>
-  static void Set(RegType value)
-  {
-    assert(value < (1U << (size - 1))) ;
+		static constexpr RegType Offset = offset;
+		static constexpr RegType Size = size;
+		static constexpr RegType Mask = (size < sizeof(RegType)) ? (static_cast<RegType>(
+						(static_cast<RegType>(1U) << size) - static_cast<RegType>(1U))) : std::numeric_limits<RegType>::max();
+		using Access = AccessMode;
 
+		//Метод устанавливает значение битового поля, только в случае, если оно достпуно для записи
+		__forceinline template<typename T = AccessMode, class = typename std::enable_if_t<std::is_base_of<ReadWriteMode, T>::value>>
+		static void Set(RegType value)
+		{
+				assert(value < (1U << (size - 1)));
 
-    RegType newRegValue = *reinterpret_cast<volatile RegType *>(Reg::Address) ; //Сохраняем текущее значение регистра
-    
-    newRegValue &= ~ (Mask << offset); //Вначале нужно очистить старое значение битового поля
-    newRegValue |= (value << offset) ; // Затем установить новое
-    
-    *reinterpret_cast<volatile RegType *>(Reg::Address) = newRegValue ; //И записать новое значение в регистр
-  }
+				RegType newRegValue = *reinterpret_cast<volatile RegType*>(Reg::Address); //Сохраняем текущее значение регистра
+
+				newRegValue &= ~(Mask << offset); //Вначале нужно очистить старое значение битового поля
+				newRegValue |= (value << offset); // Затем установить новое
+
+				*reinterpret_cast<volatile RegType*>(Reg::Address) = newRegValue; //И записать новое значение в регистр
+		}
+
+		//Метод устанавливает значение битового поля, только в случае, если оно достпуно для записи
+		__forceinline template<RegType value, typename T = AccessMode, class = typename std::enable_if_t<std::is_base_of<ReadWriteMode, T>::value>>
+		static void Set()
+		{
+				static_assert((value < (1U << (size - 1))), "Value type size is more then the field size");
+
+				RegType newRegValue = *reinterpret_cast<volatile RegType*>(Reg::Address); //Сохраняем текущее значение регистра
+
+				newRegValue &= ~(Mask << offset); //Вначале нужно очистить старое значение битового поля
+				newRegValue |= (value << offset); // Затем установить новое
+
+				*reinterpret_cast<volatile RegType*>(Reg::Address) = newRegValue; //И записать новое значение в регистр
+		}
+
 
 //  //Метод устанавливает значение битового поля, только в случае, если оно достпуно для записи
 //  __forceinline template<typename T = AccessMode,
@@ -52,24 +67,28 @@ struct RegisterField
 //    ) ;
 //  }
 
-  //Метод устанавливает значение битового поля, только в случае, если оно достпуно для записи
-  __forceinline template<typename T = AccessMode,
-          class = typename std::enable_if_t<std::is_base_of<WriteMode, T>::value>>
-  static void Write(RegType value)
-  {
-    assert(value < (1U << (size - 1))) ;
-    *reinterpret_cast<volatile RegType *>(Reg::Address) = (value << offset) ;
-  }
-  
-  
-  //Метод устанавливает проверяет установлено ли значение битового поля
-  __forceinline template<typename T = AccessMode,
-          class = typename std::enable_if_t<std::is_base_of<ReadMode, T>::value ||
-                                            std::is_base_of<ReadWriteMode, T>::value>>
-  inline static RegType Get()
-  {
-    return ((*reinterpret_cast<volatile RegType *>(Reg::Address)) &
-            (Mask << offset)) >> offset ;
-  }
+		//Метод устанавливает значение битового поля, только в случае, если оно достпуно для записи
+		__forceinline template<typename T = AccessMode, class = typename std::enable_if_t<std::is_base_of<WriteMode, T>::value>>
+		static void Write(RegType value)
+		{
+				assert(value < (1U << (size - 1)));
+				*reinterpret_cast<volatile RegType*>(Reg::Address) = (value << offset);
+		}
+
+		//Метод устанавливает значение битового поля, только в случае, если оно достпуно для записи
+		__forceinline template<RegType value, typename T = AccessMode, class = typename std::enable_if_t<std::is_base_of<WriteMode, T>::value>>
+		static void Write()
+		{
+				static_assert((value < (1U << (size - 1))), "Value type size is more then the field size") ;
+				*reinterpret_cast<volatile RegType*>(Reg::Address) = (value << offset);
+		}
+
+		//Метод устанавливает проверяет установлено ли значение битового поля
+		__forceinline template<typename T = AccessMode, class = typename std::enable_if_t<
+						std::is_base_of<ReadMode, T>::value || std::is_base_of<ReadWriteMode, T>::value>>
+		inline static RegType Get()
+		{
+				return ((*reinterpret_cast<volatile RegType*>(Reg::Address)) & (Mask << offset)) >> offset;
+		}
 };
 
