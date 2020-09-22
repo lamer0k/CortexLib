fb7d302b39a98b49aa50e27c813476bf193bc166
lamer0k <sergey.kolody@gmail.com>
2020-09-22 16:51:02 +0500

    Add rtosconfig.h

---
+++ rtos/Source/CortexM/rtosconfig.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "scbregisters.hpp"  // for SCB
+#include "susudefs.hpp"      // for forceinline 
+inline constexpr bool CORTEXM = true;
+
+__forceinline inline static void IsrExitProceed()
+{
+  SCB::ICSR::PENDSVSET::PendingState::Set(); 
+}
